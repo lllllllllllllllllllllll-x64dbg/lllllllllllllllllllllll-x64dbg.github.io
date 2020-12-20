ceab90b4b3cb706259bee89b916717a1cca036a8
Bababoi <unconfigured@null.spigotmc.org>
2020-12-20 21:08:06 +0200

    .

--- js/nyan.js
+++ js/nyan.js
@@ -1,13 +1,10 @@
-console.log('Nyan!')
+console.log('for da lulz')
 
-function cycleFrames (_nyanCat, _currentFrame) {
-	_nyanCat.classList = []
-	_nyanCat.classList.add(`frame${_currentFrame}`)
-}
 
 function replicateSparks (_sparksRow) {
 	const numberOfRowsToCoverEntireScreen = Math.ceil(document.body.offsetHeight / _sparksRow.offsetHeight)
 	const newSparksRows = document.createElement('div')
+	const newCat = document.createElement('img')
 
 	for (let a = 0; a < numberOfRowsToCoverEntireScreen-1; a++) {
 		newSparksRows.append(_sparksRow.cloneNode(true))
@@ -17,13 +14,6 @@ function replicateSparks (_sparksRow) {
 }
 
 (function () {
-	let nyanCat = document.getElementById('nyan-cat')
-	let currentFrame = 1
-
 	replicateSparks(document.getElementsByClassName('sparks-combo')[0])
 
-	setInterval(function () {
-		currentFrame = (currentFrame % 6) + 1
-		cycleFrames(nyanCat, currentFrame)
-	}, 70)
 })()
